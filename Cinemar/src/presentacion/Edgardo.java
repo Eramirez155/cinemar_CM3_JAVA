704d6490db077e32ab543bcf8221b5dd9cd78ffc
Edgardo768 <107365188+Edgardo768@users.noreply.github.com>
2022-06-14 13:03:27 -0300

    Cree la clase edgardo
    Me presente

---
+++ Cinemar/src/presentacion/Edgardo.java
@@ -0,0 +1,10 @@
+package presentacion;
+
+public class Edgardo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Soy Edgardo y tengo 24 años");
+	}
+
+}
