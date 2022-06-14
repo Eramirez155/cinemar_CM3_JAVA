635b832c712ab8d0dd4c84e3ca85818c74bf564c
Eramirez155 <107365832+Eramirez155@users.noreply.github.com>
2022-06-14 12:45:57 -0300

    first commit

---
+++ Cinemar/src/presentacion/Magdalena.java
@@ -0,0 +1,10 @@
+package presentacion;
+
+public class Magdalena {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("soy magdalena y tengo 35 años");
+	}
+
+}
