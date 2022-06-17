ca5e7d968f1279b015bf89aacb69ecbbce65b387
Naomi Poclava <milagrito1dediciembre@gmail.com>
2022-06-17 19:32:28 -0300

    desde rama naomiRamaNueva modifique las clases del paquete usuario

--- Cinemark/src/usuario/Usuario.java
+++ Cinemark/src/usuario/Usuario.java
@@ -1,5 +1,4 @@
 package usuario;
-import java.sql.Date;
 
 public class Usuario {
 	private Integer id;
@@ -25,207 +24,71 @@ public class Usuario {
 		this.complejoHabitual = complejoHabitual;
 	}
 
-
-	
-
-
-
-
-
-
 	public Integer getId() {
 		return id;
 	}
-
-
-
-
-
-
-
-
-
+	
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
-
-
-
-
-
-
-
 
 	public String getNombre() {
 		return nombre;
 	}
 
-
-
-
-
-
-
-
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
-
-
-
-
-
-
-
 
 	public String getApellido() {
 		return apellido;
 	}
 
-
-
-
-
-
-
-
-
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
 
-
-
-
-
-
-
-
-
 	public String getDni() {
 		return dni;
 	}
-
-
-
-
-
-
-
-
-
+	
 	public void setDni(String dni) {
 		this.dni = dni;
 	}
-
-
-
-
-
-
-
-
 
 	public FechaDeNacimiento getFechaDeNacimiento() {
 		return fechaDeNacimiento;
 	}
 
-
-
-
-
-
-
-
-
 	public void setFechaDeNacimiento(FechaDeNacimiento fechaDeNacimiento) {
 		this.fechaDeNacimiento = fechaDeNacimiento;
 	}
-
-
-
-
-
-
-
-
 
 	public String getEmail() {
 		return email;
 	}
 
-
-
-
-
-
-
-
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
-
-
-
-
-
-
-
 	public String getGenero() {
 		return genero;
 	}
-
-
-
-
-
-
-
-
 
 	public void setGenero(String genero) {
 		this.genero = genero;
 	}
 
 
-
-
-
-
-
-
-
 	public String getComplejoHabitual() {
 		return complejoHabitual;
 	}
-
-
-
-
-
-
-
-
 
 	public void setComplejoHabitual(String complejoHabitual) {
 		this.complejoHabitual = complejoHabitual;
 	}
 	
-	
-
-
-
-
-
-
-
-
 	@Override
 	public String toString() {
 		return "Usuario [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni
@@ -233,14 +96,7 @@ public class Usuario {
 				+ ", complejoHabitual=" + complejoHabitual + "]";
 	}
 
-
-
-
-
-
-
-
-
+	
 	public static void main(String[] args) {
 		FechaDeNacimiento nacimiento= new FechaDeNacimiento(6,11,2005);
 		System.out.println(nacimiento.toString());
