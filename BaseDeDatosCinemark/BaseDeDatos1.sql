95e703e7a745d71fe46c83f3c9eb0ed326370b7a
Edgardo768 <107365188+Edgardo768@users.noreply.github.com>
2022-06-26 14:02:36 -0300

    Modificamos la base de datos

--- BaseDeDatosCinemark/BaseDeDatos1.sql
+++ BaseDeDatosCinemark/BaseDeDatos1.sql
@@ -2,18 +2,22 @@ DROP DATABASE IF EXISTS Cinemark;
 CREATE DATABASE Cinemark CHARACTER SET utf8mb4;
 USE Cinemark;
 
-CREATE TABLE sesion(
-id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-id_pelicula VARCHAR (20),
-fecha DATE,
-horario VARCHAR (10)
-); 
 
+CREATE TABLE tipo_pelicula(
+id_tipo_pelicula INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+idioma VARCHAR(15),
+formato VARCHAR(3),
+subtitulada BOOLEAN
+);
 
+CREATE TABLE Clasificacion(
+id_clasificacion INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+identificador INTEGER,
+descripcion VARCHAR (6)
+);
 
-
-CREATE TABLE pelicula(
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Pelicula(
+id_pelicula INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 titulo_Pelicula VARCHAR(15),
 sinopsis VARCHAR(100),
 genero VARCHAR(10),
@@ -22,84 +26,109 @@ actores VARCHAR (100),
 director VARCHAR (15),
 id_tipo_pelicula INT NOT NULL,
 id_clasificacion INT NOT NULL,
-FOREIGN KEY pelicula (id_tipo_pelicula) REFERENCES tipo_pelicula (id),
-FOREIGN KEY pelcula (id_clasificacion) REFERENCES clasificacion(id)
+FOREIGN KEY (id_tipo_pelicula) REFERENCES tipo_pelicula (id_tipo_pelicula),
+FOREIGN KEY (id_clasificacion) REFERENCES clasificacion(id_clasificacion)
 );
 
 
-
-
-CREATE TABLE tipo_pelicula(
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-idioma VARCHAR(15),
-formato VARCHAR(3)
-);
-
-
-CREATE TABLE clasificacion(
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-descripcion VARCHAR (6)
-);
-
-
-CREATE TABLE pago(
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-email VARCHAR(20) NOT NULL,
-nombreDelTitular VARCHAR(20) NOT NULL,
-dniDelTitular VARCHAR(8) NOT NULL,
-numeroDeTarjeta INT(16) UNSIGNED NOT NULL,
-codigoDeSeguridad INT (3) UNSIGNED NOT NULL,
-fechaDeVencimiento DATE NOT NULL,
-aceptarTerminos BOOLEAN NOT NULL
-);
-
-
-CREATE TABLE butaca(
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-fila VARCHAR (1) NOT NULL,
-numero INT (2) NOT NULL,
-disponibilidad BOOLEAN,
-id_pago INT NOT NULL,
-FOREIGN KEY butaca (id_pago) REFERENCES pago (id)
-);
-
-
-
-ALTER TABLE butaca ADD id_sala INT NOT NULL,
-ADD FOREIGN KEY  (id_sala) REFERENCES sala (id);
-
-CREATE TABLE sala(
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-numero INT (1) NOT NULL,
-capacidad INT (3) NOT NULL
-);
-
-
-
-CREATE TABLE reserva(
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-cantidadDeEntradas INT NOT NULL,
-id_sala INT NOT NULL,
-id_descuentos INT NOT NULL,
-FOREIGN KEY  (id_sala) REFERENCES sala (id),
-FOREIGN KEY reserva (id_descuentos) REFERENCES descuentos (id)
-);
-
-
-
-CREATE TABLE descuentos(
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-dias VARCHAR (7) NOT NULL,
-membresia BOOLEAN NOT NULL,
+CREATE TABLE Descuento(
+id_descuento INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+dia VARCHAR (7) NOT NULL,
 porcentaje FLOAT
 );
 
-CREATE TABLE comprobnte(
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-fecha TIME,
+
+CREATE TABLE Reserva(
+id_reserva INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+cantidadDeEntradas INT NOT NULL,
+id_sesion INT NOT NULL,
+id_butaca INT NOT NULL,
+id_descuento INT NOT NULL,
+id_tarjetaCredito INT NOT NULL,
+id_usuario INT NOT NULL,
+FOREIGN KEY  (id_descuento) REFERENCES  Descuento (id_descuento)
+-- FOREIGN KEY  (id_butaca) REFERENCES Butaca (id_butacas)
+-- FOREIGN KEY (id_) REFERENCES descuentos (id)
+);
+
+CREATE TABLE Butaca(
+id_butacas INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+fila VARCHAR (1) NOT NULL,
+numero INT (2) NOT NULL,
+reservada BOOLEAN,
+id_sala INT NOT NULL
+-- FOREIGN KEY (id_sala) REFERENCES Sala (id_sala)
+);
+
+CREATE TABLE Sala(
+id_sala INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+numero INT (1) NOT NULL,
+formato VARCHAR (2),
+capacidad INT (3) NOT NULL,
+id_butacas INT NOT NULL,
+FOREIGN KEY (id_butacas) REFERENCES Butaca (id_butacas)
+);
+
+CREATE TABLE Sesion(
+id_sesion INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+fecha DATE,
+id_pelicula INT NOT NULL,
+id_sala INT NOT NULL,
+FOREIGN KEY (id_pelicula) REFERENCES Pelicula (id_pelicula),
+FOREIGN KEY (id_sala) REFERENCES Sala (id_sala)
+); 
 
 
-)
+
+CREATE TABLE Usuario(
+id_usuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR(50) NOT NULL,
+apellido VARCHAR(50) NOT NULL,
+email VARCHAR(50) NOT NULL,
+fecha_nacimiento DATE NOT NULL,
+dni INT
+); 
+
+
+CREATE TABLE TarjetaCredito(
+id_tarjetaCredito INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+numero INT NOT NULL,
+balance DOUBLE NOT NULL,
+limite DOUBLE NOT NULL,
+id_usuario INT NOT NULL,
+banco VARCHAR (50) NOT NULL,
+FOREIGN KEY (id_usuario) REFERENCES Usuario (id_usuario)
+);
+
+-- ALTER Reserva ADD FOREIGN KEY (id_butaca) REFERENCES Butaca (id_butacas)
+
+
+
+
+
+
+
+
+
+
+
+-- ALTER TABLE butaca ADD id_sala INT NOT NULL,
+-- ADD FOREIGN KEY  (id_sala) REFERENCES sala (id);
+
+
+
+
+
+
+
+
+
+-- CREATE TABLE comprobnte(
+-- id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+-- fecha TIME,
+
+
+-- )
 
 
 
