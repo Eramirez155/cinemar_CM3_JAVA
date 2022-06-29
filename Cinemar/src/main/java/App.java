f67a901779147735c838c830d88825178646a74b
Juarez Enzo <49879701+enzzo19@users.noreply.github.com>
2022-06-29 11:37:51 -0300

    Add App.java

--- Cinemar/src/main/java/App.java
+++ Cinemar/src/main/java/App.java
@@ -1,20 +1,20 @@
+package cinemar;
 
-
-import static spark.Spark.*;
-
-
-
-import org.apache.log4j.PropertyConfigurator;
 import com.google.gson.Gson;
+import static spark.Spark.*;
+import org.apache.log4j.PropertyConfigurator;
 
-public class App {
-	 public static void main(String[]args){
-		   String log4jConfPath = "C:/Users/enzzo/eclipse-workspace/Curso_Java/cinemar/to/log4j.properties";
-		   PropertyConfigurator.configure(log4jConfPath);
-	       Gson mapper= new Gson();
-	       // port(8080);
-	       Date fechaDeNacimiento= new Date(122, 6,23, 2, 30, 30);
-	       get("/saludo", (req,res) -> new Usuario(1,"Jose", "Perez", false, "joseperez@gmail.com", fechaDeNacimiento, "30222380" ), mapper::toJson);
-	   }
-
-}
+public class App{
+   public static void main(String[]args){
+	   String log4jConfPath = "C:/Users/enzzo/eclipse-workspace/Curso_Java/cinemar/to/log4j.properties"; // cambien el path
+	   PropertyConfigurator.configure(log4jConfPath);
+       Gson mapper= new Gson();
+       // port(8080);
+       Clasificacion clasi = new Clasificacion(1, 14, "ESta pelicula es apta para mayores de +14 años");
+       TipoPelicula tipo = new TipoPelicula(1, "Espanish", "3D", false);
+       Pelicula mi_peli = new Pelicula(1, "Jurasick Park", "Aparecen dinosaurios", "accion_fantasia", "120", "Brad Pit", "Spilver", tipo, clasi);
+       get("/info_pelicula", (req,res) -> mi_peli, mapper::toJson);
+       get("/tipo_pelicula", (req,res) -> mi_peli.getId_tipo_pelicula(), mapper::toJson);
+       get("/clasificacion_pelicula", (req,res) -> mi_peli.getId_clasificacion(), mapper::toJson);
+   }
+} 
