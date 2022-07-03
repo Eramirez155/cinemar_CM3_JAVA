c0d3e479f068d7ce335ebabfcb750ce114d3f32f
Edgardo768 <107365188+Edgardo768@users.noreply.github.com>
2022-07-02 22:50:50 -0300

    añadimos un formulario

--- Supermark/src/main/java/App.java
+++ Supermark/src/main/java/App.java
@@ -5,13 +5,31 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String log4jConfPath = "D:\\Familia\\Documentos\\Emanuel\\Proyecto1000Programadores\\cinemar_CM3_JAVA\\Supermark\\to\\log4j.properties";
+		String log4jConfPath = "E:\\cinemar_CM3_JAVA\\Supermark\\to\\log4j.properties";
 		   PropertyConfigurator.configure(log4jConfPath);
 		port(8080);
 		
 		Gson mapper = new Gson();
 		
-		get ("/saludar",(request, response)-> "Hola mundo, magdalena"); 
+		get ("/formulario",(request, response)-> "<!DOCTYPE html>\r\n"
+				+ "<html lang=\"en\">\r\n"
+				+ "<head>\r\n"
+				+ "    <meta charset=\"UTF-8\">\r\n"
+				+ "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\r\n"
+				+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n"
+				+ "    <title>Document</title>\r\n"
+				+ "</head>\r\n"
+				+ "<body>\r\n"
+				+ "<form action=\"\" method=\"get\">\r\n"
+				+ "    <input type=\"text\" name=\"\" id=\"\" placeholder=\"ingresa tu nombre:\">\r\n"
+				+ "    <input type=\"text\" name=\"\" id=\"\" placeholder=\"ingresa tu apellido:\">\r\n"
+				+ "    <input type=\"text\" name=\"\" id=\"\" placeholder=\"ingresa tu dni:\">\r\n"
+				+ "    <input type=\"text\" name=\"\" id=\"\" placeholder=\"ingresa tu email:\">\r\n"
+				+ "    <input type=\"text\" name=\"\" id=\"\" placeholder=\"ingresa tu password:\">\r\n"
+				+ "    <input type=\"submit\" value=\"Enviar\">\r\n"
+				+ "</form>\r\n"
+				+ "</body>\r\n"
+				+ "</html>"); 
 		
 		get ("/persona",(request, response)-> new Persona("Magdalena", "Galvan"), mapper::toJson);
 	}
