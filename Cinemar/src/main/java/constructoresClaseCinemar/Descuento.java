0fd1ac2b013f4641572009ede88ade5a629273d1
Eramirez155 <107365832+Eramirez155@users.noreply.github.com>
2022-07-05 13:46:23 -0300

    creamos metodos a la clase descuento

--- Cinemar/src/main/java/constructoresClaseCinemar/Descuento.java
+++ Cinemar/src/main/java/constructoresClaseCinemar/Descuento.java
@@ -1,5 +1,15 @@
 package constructoresClaseCinemar;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.MessageFormat;
+
+
+
+import credencialesGlobales.Credenciales;
+
 public class Descuento {
 	 	private int id_descuento;
 	    private String dia;
@@ -53,7 +63,66 @@ public class Descuento {
 			return "Descuento [id_descuento=" + id_descuento + ", dia=" + dia + ", porcentaje=" + porcentaje
 					+ ", precioFinal=" + precioFinal + "]";
 		}
-
+	public static void cambia_descuento(String dia, double porcentaje) {
+		
+		//  Registrar JDBC Driver
+				// JDBC nombre del driver y URL de la BDD
+					Credenciales mi_credi = new Credenciales();
+					String JDBC_DRIVER = mi_credi.JDBC_DRIVER; 
+					String DB_URL = mi_credi.DB_URL;
+				 // Credenciales de la BDD
+					String USER = mi_credi.USER;
+					String PASS = mi_credi.PASS;
+					
+					Connection conn = null;
+					 Statement stmt = null;
+					 
+					 try{
+					 //PASO 2: Registrar JDBC driver
+					 Class.forName(JDBC_DRIVER);
+					 
+					 //PASO3: Abrir una Conexion
+					 System.out.println("Connecting to database...");
+					 conn = DriverManager.getConnection(DB_URL,USER,PASS);
+					 
+					 //PASO 4: Ejecutar una consulta SQL
+					 System.out.println("Creating statement...");
+					 stmt = conn.createStatement();
+					 String sql;
+					 String porcentaje_STR = String.valueOf(porcentaje).replace(",", ".");
+					 sql = MessageFormat.format("UPDATE descuento SET porcentaje = {0} WHERE dia = {1}", porcentaje_STR, dia );
+					 System.out.println(sql);
+					 stmt.executeUpdate(sql);
+					 
+					 System.out.println("Valores ACTUALIZADOS con exito :) !");
+					 
+					 //PASO6: Entorno de Limpieza
+					 stmt.close();
+					 conn.close();
+					 }catch(SQLException se){
+						 // Resolver errores para JDBC
+						 se.printStackTrace();
+					 }catch(Exception e){
+						 // Resolver errores para Class.forName
+						 e.printStackTrace();
+					 }finally{
+					 // Bloque finalmente utilizado para cerrar recursos
+					 try{
+						 if(stmt!=null)
+							 stmt.close();
+					 }catch(SQLException se2){
+					 }// Nada que podamos hacer
+					 try{
+						 if(conn!=null)
+							 conn.close();
+					 }catch(SQLException se){
+					 se.printStackTrace();
+					 	} //cierra finally try
+					 } //cierra try
+					 System.out.println("Goodbye!");
+			
+		}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
---
+++ Cinemar/src/main/java/constructoresClaseCinemar/cinemar.java
@@ -0,0 +1,14 @@
+package constructoresClaseCinemar;
+
+public class cinemar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Descuento mi_dec = new Descuento();
+		mi_dec.cambia_descuento("'domingo'", 0.9);
+
+	}
+
+}
+
